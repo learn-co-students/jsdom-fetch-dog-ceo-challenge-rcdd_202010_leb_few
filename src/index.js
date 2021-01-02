7029c09c413ac953295b988448f9e2309a4917b6
Abdulrahman Ghalayini <ghalayini.abdulrahman.17@gmail.com>
2021-01-02 10:39:48 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -14,27 +14,27 @@ const dropdown = document.querySelector("#breed-dropdown")
 // if they don't start with the letter, hide them with CSS
 
 // State
-let breedList = []
+let breedList = [];
 
 // added the event listener to the dropdown
 dropdown.addEventListener("change", event => {
   // got the selected value
-  const letter = event.target.value
+  const letter = event.target.value;
 
   // filter the array
   const filteredBreeds = breedList.filter(function(breed) {
-    return breed.startsWith(letter)
-  })
+    return breed.startsWith(letter);
+  });
   
   // (also, clear the list)
-  dogList.innerHTML = ''
+  dogList.innerHTML = '';
 
   // use that list to display
   filteredBreeds.forEach(function(breed) {
-    renderBreed(breed)
-  })
+    renderBreed(breed);
+  });
 
-  console.log(filteredBreeds)
+  console.log(filteredBreeds);
 
   // iterate over all the list items
   // dogList.querySelectorAll("li").forEach(li => {
@@ -46,32 +46,32 @@ dropdown.addEventListener("change", event => {
   //     li.style.display = ""
   //   }
   // })
-})
+});
 
 // find the closest parent
 dogList.addEventListener("click", event => {
   // use a condition to figure out what element was clicked
   if (event.target.matches("li")) {
     // run our DOM logic
-    event.target.style.color = "red"
+    event.target.style.color = "red";
   }
-})
+});
 
 function renderImage(imageUrl) {
-  const img = document.createElement("img")
-  img.src = imageUrl
-  const imageContainer = document.querySelector("#dog-image-container")
-  imageContainer.append(img)
+  const img = document.createElement("img");
+  img.src = imageUrl;
+  const imageContainer = document.querySelector("#dog-image-container");
+  imageContainer.append(img);
 }
 
 function renderAllImages(data) {
   // when we have the data
-  const images = data.message
+  const images = data.message;
     
   // append to the DOM
   images.forEach(imageUrl => {
-    renderImage(imageUrl)
-  })
+    renderImage(imageUrl);
+  });
 }
 
 function loadImages() {
@@ -79,14 +79,14 @@ function loadImages() {
   // make a GET request
   fetch("https://dog.ceo/api/breeds/image/random/4")
     .then(response => response.json())
-    .then(renderAllImages)
+    .then(renderAllImages);
 }
 
 
 function renderBreed(breed) {
-  const li = document.createElement("li")
-  li.textContent = breed
-  dogList.append(li)
+  const li = document.createElement("li");
+  li.textContent = breed;
+  dogList.append(li);
 }
 
 function loadBreeds() {
@@ -95,13 +95,13 @@ function loadBreeds() {
     .then(data => {
       // Object.keys
       // for..in
-      breedList = Object.keys(data.message)
+      breedList = Object.keys(data.message);
 
       breedList.forEach(breed => {
-        renderBreed(breed)
-      })
-    })
+        renderBreed(breed);
+      });
+    });
 }
 
-loadBreeds()
-loadImages()
+loadBreeds();
+loadImages();
